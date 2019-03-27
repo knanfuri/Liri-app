62a9f2e8bb06c436b8d133aae4808c05f094aa0a
knanfuri <knanfuri@yahoo.com>
2019-03-26 21:48:40 -0500

    updates

--- Liri.js
+++ Liri.js
@@ -1,3 +1,19 @@
 require("dotenv").config();
+var axios = require('axios');
+console.log();
+
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+
+axios.get('/user', {
+    params: {
+      ID: 12345
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
---
+++ main.js
@@ -0,0 +1,55 @@
+// var movieName = "";
+// movieName = process.argv;
+// console.log(movieName);
+// movieName.shift();
+// movieName.shift();
+// movieName.join("_");
+// console.log(movieName);
+
+// INSTRUCTIONS:
+// ---------------------------------------------------------------------------------------------------------
+// Level 1:
+// Take any movie with a word title (ex: Cinderella) as a Node argument and retrieve the year it was created
+var movie = process.argv[2];
+var axios = require('axios');
+
+// Level 2 (More Challenging):
+// Take a move with multiple words (ex: Forrest Gump) as a Node argument and retrieve the year it was created.
+// ---------------------------------------------------------------------------------------------------------
+
+// Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
+// ...
+
+
+// Grab or assemble the movie name and store it in a variable called "movieName"
+var movieName = movie;
+console.log(movieName);
+// ...
+
+
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+
+// Then create a request with axios to the queryUrl
+// ...
+axios.get(queryUrl).then(function (response) {
+    //console.log(response);
+    console.log(response.data.Title);
+    console.log(`Release Date: ${response.data.Released}`);
+    console.log(`${response.data.Title}'s Metascore is:${response.data.Metascore} `);
+    console.log(response.data.Plot);
+
+
+
+})
+
+// If the request with axios is successful
+// ...
+
+// Then log the Release Year for the movie
+// ...
